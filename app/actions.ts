c4a37ada7746f41485a28a310ace10b8f0a5d166
mukhut <mukhutjp@gmail.com>
2025-07-15 16:39:54 +0100

    modif

---
+++ app/actions.ts
@@ -0,0 +1,30 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+import { Prisma } from "./generated/prisma";
+
+export async function checkAndAddUser(email:string,name:string){
+    if(!email) return;
+
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where :{
+                email:email
+            }
+        })
+
+        if(!existingUser && name){
+            await prisma.user.create({
+                data: {
+                    email,
+                    name
+                }
+            })
+        }
+        
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+ 
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,28 @@
 "use client"
+import { UserButton, useUser } from '@clerk/nextjs';
+import { Layers } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { checkAndAddUser } from '../actions';
 
 function Navbar() {
     const pathname = usePathname()
+    const {user} = useUser()
     const navLink = [
         {
            href:"/",
            label: "Facture" 
         },
     ]
+
+    useEffect(()=>{
+        if(user?.primaryEmailAddress?.emailAddress && user.fullName){
+            checkAndAddUser(user?.primaryEmailAddress?.emailAddress,user.fullName)
+        }
+
+     },
+     [user])
 
     const isActiveLink = (href:string) =>  
         pathname.replace(/\/$/,"") === href.replace(/\/$/,"");
@@ -28,10 +40,20 @@ function Navbar() {
     
   return (
     <div>
-        <div className=''>
-            <div>
-                <div>
+        <div className='border-b border-base-300 px-5 md:px-[10%] py-4'>
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center'>
+                <div className='bg-accent-content text-accent rounded-full p-2'>
+                   <Layers className='w-6 h-6'/> 
+                </div>
+                <span className='ml-3 font-bold text-2xl italic'>
+                    In <span className='text-accent'>Voice</span>
+                </span>
+                </div>
+                
+                <div className='flex space-x-4 items-center'>
                     {renderLiks("btn")}
+                    <UserButton/>
                 </div>
 
             </div>
