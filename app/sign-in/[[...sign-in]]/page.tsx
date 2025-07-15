ff57fbb9eb860b356fc7219e9af4f3a8ed36017a
mukhut <mukhutjp@gmail.com>
2025-07-15 23:01:10 +1300

    first commit

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,18 @@
+import { SignIn } from '@clerk/nextjs'
+import React from 'react'
+
+function page() {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+            <div className="max-w-md">
+                 <SignIn />
+            </div>
+            
+        </div>
+     
+    </div>
+  )
+}
+
+export default page
